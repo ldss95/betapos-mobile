cb9d70ac977849339f4e99a6e5088e7542df467a
Luis Santiago <lsantiago@nd.com.do>
2024-01-08 13:57:18 -0400

    feat: crea UI de pantalla "Seguridad y Contraseña"

--- src/screens/Config/screens/SecurityAndPassword.tsx
+++ src/screens/Config/screens/SecurityAndPassword.tsx
@@ -1,9 +1,100 @@
-import { ScreenContainer } from '@/components';
+import { TouchableOpacity, Image, Text, View, Dimensions, StyleSheet, ImageSourcePropType } from 'react-native';
+
+import { BackButton, RenderIf, ScreenContainer } from '@/components';
+import Colors from '@/constants/Colors';
+import Space from '@/constants/Space';
+
+const {  width } = Dimensions.get('screen');
+const ItemSize = (width - 60) / 2;
 
 export default function SecurityAndPasswordScreen() {
 	return (
 		<ScreenContainer>
-			
+			<BackButton />
+
+			<View style={styles.container}>
+				<Item
+					title={'Cambiar\nContraseña'}
+					icon={require('@/assets/icons/password-check.png')}
+				/>
+				<Item
+					title='Activar Huella'
+					description='Inicia sesión mas fácil y rápido'
+					icon={require('@/assets/icons/finger-scan.png')}
+				/>
+				<Item
+					title='Eliminar Cuenta'
+					description='Esta acción es permanente'
+					icon={require('@/assets/icons/danger.png')}
+					danger
+				/>
+			</View>
 		</ScreenContainer>
 	)
 }
+
+interface ItemProps {
+	title: string;
+	description?: string;
+	icon: ImageSourcePropType;
+	danger?: boolean;
+}
+
+const Item = ({ title, description, icon, danger }: ItemProps) => (
+	<TouchableOpacity
+		style={[
+			styles.itemContainer,
+			{
+				...danger && {
+					borderWidth: 2,
+					borderColor: '#C32A2A'
+				}
+			}
+		]}
+		onPress={() => null}
+	>
+		<Image
+			source={icon}
+			style={{ height: 56, width: 56 }}
+		/>
+
+		<View>
+			<Text
+				style={[styles.itemTitle, { ...danger && { color: '#C32A2A' } }]}
+			>
+				{title}
+			</Text>
+			<RenderIf condition={!!description}>
+				<Text style={styles.itemDescription}>{description}</Text>
+			</RenderIf>
+		</View>
+	</TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		gap: 20
+	},
+	itemContainer: {
+		backgroundColor: Colors.BgCard,
+		borderRadius: Space.BorderSm,
+		width: ItemSize,
+		height: ItemSize,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingVertical: 30
+	},
+	itemTitle: {
+		color: '#FFF',
+		fontSize: 14,
+		textAlign: 'center'
+	},
+	itemDescription: {
+		color: '#B0B0B0',
+		fontSize: 10,
+		textAlign: 'center',
+		fontWeight:'400'
+	}
+});
