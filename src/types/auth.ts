bca60ec38b489d4c87b5f08a3e004c4b2c1d8fe5
Luis Santiago <lsantiago@nd.com.do>
2024-01-13 18:00:36 -0400

    feat: ajusta tipo de la sesion

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,9 +1,12 @@
 export interface SessionProps {
+	id: string;
 	firstName: string;
 	lastName: string;
 	email: string;
+	nickName?: string;
+	gender: 'F' | 'M' | 'O';
+	birthDate: string;
 	roleId: string;
-	id: string;
 	businessId: string;
 	photoUrl: string | null;
 	roleCode: RoleCode;
